270ec24d717571f461a576a68a7ab811dd8b84da
arronHealy <G00346883@gmit.ie>
2018-10-15 12:16:32 +0100

    first commit

---
+++ src/app/create/create.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import {PostService} from '../services/post.service';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
+})
+export class CreateComponent implements OnInit {
+
+  constructor(private service:PostService) { }
+
+  onAddPost(form: NgForm) {
+
+    this.service.addPost(form.value.title, form.value.content);
+    
+    console.log(form.value);
+    form.resetForm();
+  }
+
+
+  ngOnInit() {
+
+
+
+  }
+
+}
